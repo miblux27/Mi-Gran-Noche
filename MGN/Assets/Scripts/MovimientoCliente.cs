4a2cc384598c5f72963057cc201e9ed2fc92bbfb
ScampiStar <44586245+ScampiStar@users.noreply.github.com>
2018-11-14 15:41:32 +0100

    Script del movimiento de los clientes
    Está puesto que se mueva la puerta, falta añadir los sprites de los clientes con sus animaciones

---
+++ MGN/Assets/Scripts/MovimientoCliente.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoCliente : MonoBehaviour {
+
+	public float velocidad;
+	public float tiempo;
+	private float contador;
+	private int rebota = 1;
+
+	private void OnTriggerEnter2D(Collider2D collision){
+		if(collision.GetComponent<Collider2D>().CompareTag("Punto"))
+		{
+			rebota = -rebota;
+		}
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		contador += Time.deltaTime;
+
+		if (contador >= tiempo && rebota > 0)
+		{
+			Debug.Log("voy palante");
+			this.GetComponent<Rigidbody2D>().velocity = new Vector3(velocidad, 0 , 0);
+		}
+		else 
+			if(contador >= tiempo && rebota < 0)
+			{
+				Debug.Log("voy izquierda");
+				this.GetComponent<Rigidbody2D>().velocity = new Vector3(-velocidad, 0 , 0);
+			}
+	}
+}
