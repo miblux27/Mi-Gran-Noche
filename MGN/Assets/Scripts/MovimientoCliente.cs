aedb310b2a4a5c7572c38abaa33f3d06eaf57dc8
ScampiStar <44586245+ScampiStar@users.noreply.github.com>
2018-11-14 21:03:22 +0100

    Spawner clientes y movimiento clientes
    Mi parte del sprint 2 que incluyo:
    -Spawner de clientes
    -Movimiento de los clientes
    -Animaciones incompletas pero está casi hecho

--- MGN/Assets/Scripts/MovimientoCliente.cs
+++ MGN/Assets/Scripts/MovimientoCliente.cs
@@ -5,35 +5,90 @@ using UnityEngine;
 public class MovimientoCliente : MonoBehaviour {
 
 	public float velocidad;
-	public float tiempo;
-	private float contador;
+
+	private float auxVelocidad;
+
+	public float pedirRate;
+	public float idleRate;
+	public float tiempoPedir;
+	public float tiempoIdle;
 	private int rebota = 1;
+	private Random random = new Random();
+	private int pasarIDLE;
+	private Animator animator;
+	private bool mirandoDerecha = true;
 
 	private void OnTriggerEnter2D(Collider2D collision){
-		if(collision.GetComponent<Collider2D>().CompareTag("Punto"))
+		if(collision.GetComponent<Collider2D>().CompareTag("Punto")) //Cambia de direccion
 		{
+			flip();
 			rebota = -rebota;
 		}
 	}
+
+	private void Mover()
+	{
+		
+		if (rebota > 0)
+		{
+			Debug.Log("voy palante");
+			this.GetComponent<Rigidbody2D>().velocity = new Vector2(velocidad, 0);
+		}
+		else 
+			if(rebota < 0)
+			{
+				Debug.Log("voy izquierda");
+				this.GetComponent<Rigidbody2D>().velocity = new Vector2(-velocidad, 0);
+			}
+		animator.SetFloat("speed", (this.GetComponent<Rigidbody2D>().velocity.x != 0) ? 1f : -1f); //Cambia de animacion
+
+		
+	}
+	private void pedir()
+	{
+		velocidad = 0;
+		Debug.Log("He pasado a pedir");
+		animator.SetBool("pedir", true);
+		Invoke("pararPedir", tiempoPedir);
+	}
+
+	private void pararPedir()
+	{
+		Debug.Log("paro de pedir");
+		animator.SetBool("pedir", false);
+		velocidad = auxVelocidad;
+	}
+
+	private void idle()
+	{
+		velocidad = 0;
+		Debug.Log("He pasado al idle");
+		Invoke("pararIdle", tiempoIdle);
+	}
+
+	 private void pararIdle()
+	{
+		velocidad = auxVelocidad;
+	}
+	private void flip()
+    {
+        mirandoDerecha = !mirandoDerecha;
+        Vector3 escala = transform.localScale;
+        escala.x *= -1;
+        transform.localScale = escala;
+    }
 	// Use this for initialization
 	void Start () {
+		auxVelocidad = velocidad;
+		Debug.Log(auxVelocidad);
+		animator = GetComponent<Animator>();
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		contador += Time.deltaTime;
-
-		if (contador >= tiempo && rebota > 0)
-		{
-			Debug.Log("voy palante");
-			this.GetComponent<Rigidbody2D>().velocity = new Vector3(velocidad, 0 , 0);
-		}
-		else 
-			if(contador >= tiempo && rebota < 0)
-			{
-				Debug.Log("voy izquierda");
-				this.GetComponent<Rigidbody2D>().velocity = new Vector3(-velocidad, 0 , 0);
-			}
+		Mover();
+		InvokeRepeating("pedir", pedirRate, pedirRate);
+		InvokeRepeating("idle", idleRate, idleRate);
 	}
 }
---
+++ MGN/Assets/Scripts/SpawnerClientes.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerClientes : MonoBehaviour {
+	public GameObject cliente;
+	public float spawnTime = 3f;
+	public Transform[] spawnPoints; 
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating("Spawn", spawnTime, spawnTime);
+	}
+	
+	// Update is called once per frame
+	void Spawn () {
+		int spawnPointsIndex = Random.Range(0, spawnPoints.Length);
+		Instantiate(cliente, spawnPoints[spawnPointsIndex].position, spawnPoints[spawnPointsIndex].rotation);
+	}
+}
