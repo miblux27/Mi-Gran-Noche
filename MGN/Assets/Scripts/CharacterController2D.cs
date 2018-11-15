73575d90bcc29ce137610a13043021af5ae08421
miblux27 <al361656@uji.es>
2018-11-15 11:44:44 +0100

    Revert "Merge branch 'master' of https://github.com/miblux27/Mi-Gran-Noche"
    This reverts commit bbcef11a6d9a852e14951821191181d84f925180, reversing
    changes made to b6d7d70f3d12056ae0535191c1d15dcbf6d89828.

--- MGN/Assets/Scripts/CharacterController2D.cs
+++ MGN/Assets/Scripts/CharacterController2D.cs
@@ -19,7 +19,6 @@ public class CharacterController2D : MonoBehaviour
     private bool mirandoDerecha = true;
     private float moveInput;
     private Rigidbody2D rgbd;
-    private Collider2D[] colliders;
     private float time = 4f;
     private float dashTime;
     private bool barra;
@@ -41,7 +40,6 @@ public class CharacterController2D : MonoBehaviour
         dashTime = dashLimit;
         rgbd = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
-        colliders = GetComponents<Collider2D>();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -80,17 +78,11 @@ public class CharacterController2D : MonoBehaviour
             //tumbar collider, para pasar por debajo del enemigo || Habilitar\Deshabilitar colliders necesarios.
             time = 0;
             dashActivado = true;
-            animator.SetBool("slided", dashActivado);
-            colliders[1].enabled = dashActivado;
-            colliders[0].enabled = !dashActivado;
         }
         else if (Input.GetKeyUp(KeyCode.LeftControl))
         {
             dashTime = dashLimit;
             dashActivado = false;
-            animator.SetBool("slided", dashActivado);
-            colliders[1].enabled = dashActivado;
-            colliders[0].enabled = !dashActivado;
         }
         else if (dashActivado)
         {
