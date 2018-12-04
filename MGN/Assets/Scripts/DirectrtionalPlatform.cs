a3f436e3b555f3d0bc9e12905c9167e9d2c21a9f
Macadrian <adrianbautistagil@gmail.com>
2018-12-04 22:45:36 +0100

    Plataformas Bidireccionales
    Añadidas zonas para subir y bajar entre los pisos "Tecla S"

---
+++ MGN/Assets/Scripts/DirectrtionalPlatform.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirectrtionalPlatform : MonoBehaviour {
+
+    private PlatformEffector2D effector2D;
+    private float waitTime = 0f;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        effector2D = GetComponent<PlatformEffector2D>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKey(KeyCode.S))
+        {
+            effector2D.rotationalOffset = 180f;
+            waitTime = 0.2f;
+        }
+        else if (waitTime > 0)
+        {
+            waitTime -= Time.deltaTime;
+        }
+        else if(effector2D.rotationalOffset != 0f)
+        {
+            effector2D.rotationalOffset = 0f;
+        }
+		
+	}
+}
