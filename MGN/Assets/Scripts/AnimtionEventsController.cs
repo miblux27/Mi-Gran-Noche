e14f8b5d18d4b9fc0a3f79cf27b24b1e03f24051
Macadrian <adrianbautistagil@gmail.com>
2018-12-06 16:25:51 +0100

    Extras menores

---
+++ MGN/Assets/Scripts/AnimtionEventsController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimtionEventsController : MonoBehaviour {
+
+    public ParticleSystem sparks;
+    private void ActivarChispas()
+    {
+        sparks.gameObject.SetActive(true);
+        sparks.Play();
+    }
+}
