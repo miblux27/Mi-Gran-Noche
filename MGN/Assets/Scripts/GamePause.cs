6f98164624470d5df2316246b2eeb1d66c7e0df5
ScampiStar <al364481@uji.es>
2018-12-18 12:45:20 +0100

    Menú Pausa
    Si pulsas Esc sale el menú de pausa.
    Hay un botón de quit que no hace nada.

---
+++ MGN/Assets/Scripts/GamePause.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GamePause : MonoBehaviour
+{
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(GameManager.juegoEnPausa)
+            {
+                Resume();
+            }
+            else  Pause(); 
+        }    
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameManager.juegoEnPausa = false;
+    }
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameManager.juegoEnPausa = true;
+    }
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void QuitGame()
+    {
+        Debug.Log("Quitting game...");
+    }
+}
