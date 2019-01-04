a5f03c004f0b929fbc67d8a6931d448657d11ae1
ScampiStar <44586245+ScampiStar@users.noreply.github.com>
2019-01-04 16:48:10 +0100

    Menú principal, zona plataformas, empezando pantalla de score

---
+++ MGN/Assets/Scripts/Score Scene/PantallaPuntuacion.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class PantallaPuntuacion : MonoBehaviour
+{
+    public void SalirAlMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void ContinuarJuego()
+    {
+        SceneManager.LoadScene("Main");
+    }
+}
