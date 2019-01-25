3eecd2a96d6c57aebc42c9b507c6932977dc51a8
Macadrian <adrianbautistagil@gmail.com>
2019-01-25 18:16:41 +0100

    objeto jarra

---
+++ MGN/Assets/Scripts/CervezaArma.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CervezaArma : MonoBehaviour
+{
+    public GameObject particle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameObject instancia = Instantiate(particle, gameObject.transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
